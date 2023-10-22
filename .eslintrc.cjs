9c640300fcf5d7ac2e03c620264a1d7d2f7788a9
Huw Wilkins <huw.wilkins@canonical.com>
2023-10-23 10:40:01 +1100

    Set up Prettier.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,12 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    "plugin:prettier/recommended",
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ["prettier", 'react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,5 +1,10 @@
 const App = () => {
-  return <h1>Canonical ReBAC Admin</h1>;
+  return (
+    <h1>
+      Canonical ReBAC Admin
+      <div>Something or other</div>
+    </h1>
+  );
 };
 
 export default App;
