d4206ecf9cd712dd569ef3e8c70d200220658aba
Huw Wilkins <huw.wilkins@canonical.com>
2023-10-23 17:45:00 +1100

    Fix prettier.

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,7 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
 
 import Index from "./index";
-
 
 describe("Index", () => {
   it("renders", () => {
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
 // TODO: components, routes etc. will be exported from this file.
-export default () => "Canonical ReBAC Admin"
+export default () => "Canonical ReBAC Admin";
