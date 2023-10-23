fd63df5d77546c5ba07be2894b52e606f966efa6
huwshimi <huwshimi@users.noreply.github.com>
2023-10-24 09:16:43 +1100

    WD-6767 - Add demos setup (#12)
    * Set up docker for the demo service.

---
+++ vite-demo.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig, loadEnv, mergeConfig } from "vite";
+import libraryConfig from "./vite.config";
+
+/**
+ * This config is used for the demo service. The main Vite config sets up the
+ * package to build in library mode, but we need to serve the demo application instead.
+ */
+export default defineConfig((env) => {
+  const previewConfig = defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+    return {
+      build: {
+        // Use a different location to the library build.
+        outDir: "build",
+      },
+      preview: {
+        port: Number(env.PORT),
+      },
+    };
+  });
+  const libConfig = libraryConfig(env);
+  delete libConfig.build;
+  return mergeConfig(libConfig, previewConfig(env));
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,30 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
-export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "rebac-admin",
-      fileName: "rebac-admin",
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    build: {
+      lib: {
+        entry: resolve(__dirname, "src/index.ts"),
+        name: "rebac-admin",
+        fileName: "rebac-admin",
+      },
+      rollupOptions: {
+        external: ["react", "react-dom"],
+        output: {
+          globals: {
+            react: "React",
+            "react-dom": "ReactDOM",
+          },
         },
       },
     },
-  },
-  plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-  },
+    plugins: [react()],
+    server: {
+      host: "0.0.0.0",
+      port: Number(env.PORT),
+    },
+  };
 });
