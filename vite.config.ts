f575a4e90c579a9587f74801f0e11f39d68562cd
Huw Wilkins <huw.wilkins@canonical.com>
2023-10-24 12:16:16 +1100

    Set up absolute imports. Update TS options.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
@@ -22,7 +23,7 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
-    plugins: [react()],
+    plugins: [react(), tsconfigPaths()],
     server: {
       host: "0.0.0.0",
       port: Number(env.PORT),
