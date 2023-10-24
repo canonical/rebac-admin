78d953ed358c1fb1ea283bd69f0079482866e3db
vladimir-cucu <vladimir.cucu@canonical.com>
2023-10-24 23:05:21 +0300

    fix: Fix linting and msw import errors

--- orval.config.ts
+++ orval.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       client: "react-query",
       mode: "tags-split",
       mock: true,
-      clean: "src/api",
+      clean: ["src/api"],
       prettier: true,
     },
   },
