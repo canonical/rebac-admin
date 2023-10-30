d7fb6311c54334ca4e16b034daf42240dbc56826
huwshimi <huwshimi@users.noreply.github.com>
2023-10-30 11:22:05 +1100

    WD-6744 - Application layout (#23)
    * Add example application layout.

--- src/components/ReBACAdmin/ReBACAdmin.tsx
+++ src/components/ReBACAdmin/ReBACAdmin.tsx
@@ -1,5 +1,12 @@
 const ReBACAdmin = () => {
-  return <h1>Canonical ReBAC Admin</h1>;
+  return (
+    <div className="p-panel">
+      <div className="p-panel__header">
+        <h4 className="p-panel__title">Canonical ReBAC Admin</h4>
+      </div>
+      <div className="p-panel__content"></div>
+    </div>
+  );
 };
 
 export default ReBACAdmin;
