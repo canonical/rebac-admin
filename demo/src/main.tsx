d7e28a7337354e655749f7eff91852599fc2ff83
Vladimir Cucu <108150922+vladimir-cucu@users.noreply.github.com>
2023-10-30 09:55:59 +0300

    WD-6743 - feat: Serve mock API and set up React Query (#24)

--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -6,12 +6,24 @@ import "./scss/index.scss";
 
 const root = document.getElementById("root");
 
+const defferRender = async () => {
+  const { mockApiWorker } = await import("./mockApiWorker.ts");
+  return mockApiWorker.start();
+};
+
 if (root) {
-  ReactDOM.createRoot(root).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  );
+  defferRender()
+    .then(() => {
+      ReactDOM.createRoot(root).render(
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>,
+      );
+      return;
+    })
+    .catch((error) =>
+      console.error("Error while trying to start mock API worker.", error),
+    );
 } else {
   console.error("Root element not found in DOM.");
 }
--- src/components/ReBACAdmin/ReBACAdmin.tsx
+++ src/components/ReBACAdmin/ReBACAdmin.tsx
@@ -1,11 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 const ReBACAdmin = () => {
+  const queryClient = new QueryClient();
+
   return (
-    <div className="p-panel">
-      <div className="p-panel__header">
-        <h4 className="p-panel__title">Canonical ReBAC Admin</h4>
+    <QueryClientProvider client={queryClient}>
+      <div className="p-panel">
+        <div className="p-panel__header">
+          <h4 className="p-panel__title">Canonical ReBAC Admin</h4>
+        </div>
+        <div className="p-panel__content"></div>
       </div>
-      <div className="p-panel__content"></div>
-    </div>
+    </QueryClientProvider>
   );
 };
 
