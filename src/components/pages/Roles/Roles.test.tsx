7d2ebe96c1865e3487d54a5d78d8f68670981e49
Huw Wilkins <huw.wilkins@canonical.com>
2024-11-05 12:39:46 +1100

    fix: spurious failing test

--- src/components/pages/Roles/Roles.test.tsx
+++ src/components/pages/Roles/Roles.test.tsx
@@ -147,9 +147,6 @@ test("should display error notification and refetch data", async () => {
 });
 
 test("displays the add panel", async () => {
-  mockApiServer.use(
-    getGetRolesMockHandler(getGetRolesResponseMock({ data: [] })),
-  );
   renderComponent(
     <ReBACAdminContext.Provider value={{ asidePanelId: "aside-panel" }}>
       <aside id="aside-panel"></aside>
