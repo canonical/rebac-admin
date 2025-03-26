ad6c502b8ca0d0ce281486ecf9122f89768cbdb4
Huw Wilkins <huw.wilkins@canonical.com>
2025-03-26 15:12:34 +1100

    refactor: remove polyfill import

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
 import dts from "vite-plugin-dts";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ mode }) => {
